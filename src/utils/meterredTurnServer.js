b4b0d7ceaaa0a5a8c138ddcbb30a5732b738d9fc
ronishbrh <ronish.brh111@gmail.com>
2026-03-16 23:14:45 +0545

    changes with turn server

--- src/utils/meterredTurnServer.js
+++ src/utils/meterredTurnServer.js
@@ -2,7 +2,8 @@ export async function getMeterredTurnServers() {
   try {
     console.log('Fetching TURN credentials from backend...');
 
-    const response = await fetch('/api/turn-credentials');
+    // const response = await fetch('/api/turn-credentials');
+    const response = await fetch('https://webrtc-signaling-server-up3e.onrender.com/api/turn-credentials');
     // const response = await fetch('http://localhost:8080/api/turn-credentials');
 
     console.log('Response status:', response.status);
